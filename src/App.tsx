89bef77f0e874f49c849e5f1e1081256f8ad44ac
mahim-shariar <mdmahim924214@gmail.co>
2022-02-25 02:07:56 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Seller from './components/Seller/Seller';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Seller_details from './components/sellers-details/Seller_details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+      <Router>
+        <Routes>
+          <Route element={<Seller></Seller>} path='/' ></Route>
+          <Route element={<Seller></Seller>} path='seller' ></Route>
+          <Route element={<Seller_details></Seller_details>} path='seller/:id' ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
